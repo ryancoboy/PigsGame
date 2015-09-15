03590a1c2efdc0f3f6e28e6cc4afaa15cca83d86
jBeller <jbeller@mail.csuchico.edu>
2015-09-14 18:47:50 -0700

    Updated Dice.h

--- Dice.h
+++ Dice.h
@@ -1,12 +1,16 @@
 #ifndef __DICE__
 #define __DICE__
 
+#include <time.h>
+
 class Dice
 {
-private:
-	const int Sides = 6;
-public:
-	int RollDice();
+  private:
+	int Sides;
+  public:
+	int RollDice(){ Sides = 6; };
+
+	int Set_NumSides(int);
 };
 
 #endif

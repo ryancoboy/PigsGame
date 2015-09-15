576aa52bebafe8876be75db0c909aac897d1e403
jBeller <jbeller@mail.csuchico.edu>
2015-09-14 18:49:44 -0700

    Updated Dice.cpp

--- Dice.cpp
+++ Dice.cpp
@@ -7,3 +7,7 @@ int Dice::RollDice()
     srand(time(NULL));              // Uses the current time as the random seed
     return rand() % Sides + 1;      // Return a random number between 1 and number of sides
 }
+int Dice::Set_NumSides(int Num)
+{
+	Sides = Num;
+}

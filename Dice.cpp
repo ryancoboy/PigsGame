456ffc2e23a2fbb1353508ef3b2a6fc5a2cc6334
jBeller <jbeller@mail.csuchico.edu>
2015-09-14 18:19:29 -0700

    Commit for Dice.cpp

---
+++ Dice.cpp
@@ -0,0 +1,9 @@
+#include <cstdlib>  // For the random number generator
+#include <ctime>    // Will use time as the random seed
+#include "Dice.h"
+
+int Dice::RollDice()
+{
+    srand(time(NULL));              // Uses the current time as the random seed
+    return rand() % Sides + 1;      // Return a random number between 1 and number of sides
+}

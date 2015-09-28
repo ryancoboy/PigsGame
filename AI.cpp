80fbcd0d8c2906c23acd435d7b741cafde6c80d0
Sebastien Hutt <seb.hutt@gmail.com>
2015-09-28 11:13:54 -0700

    .

---
+++ AI.cpp
@@ -0,0 +1,17 @@
+#include "AI.h"
+
+int Player::AI::RollDie(Dice *Die)
+{
+	if(TempScore < 12)
+	{
+		//roll die
+		//add to score
+		
+	}
+	else
+	{
+		Set_Hold(true);
+	}
+	
+
+}
--- AI.h
+++ AI.h
@@ -7,7 +7,10 @@ class AI : Player
 {
 private:
 public:
-	int RollDie();
+	AI() : TempScore(0), OverallScore(0),Hold(false){}
+	int RollDie(Dice *Die);
+	
+	
 };
 
 #endif
